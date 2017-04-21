6afa59cf17f47cfd7f545503e39f4149ff812efa
t4rzsan <jakob.christensen@gmail.com>
2017-04-21 10:06:38 +0200

    Changed .gitignore to include dist, build and lib for convenience.

---
+++ CoreWebApplication2/wwwroot/build/myviewmodel.js
@@ -0,0 +1,15 @@
+"use strict";
+var ko = require("knockout");
+var $ = require("jquery");
+var MyViewModel = (function () {
+    function MyViewModel(firstname, lastname) {
+        this.firstname = ko.observable(firstname);
+        this.lastname = ko.observable(lastname);
+    }
+    return MyViewModel;
+}());
+var container = $("#myview-container");
+if (container.length == 1) {
+    ko.applyBindings(new MyViewModel("Jakob", "Christensen"), container[0]);
+}
+//# sourceMappingURL=myviewmodel.js.map
